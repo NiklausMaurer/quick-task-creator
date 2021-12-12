0a6b5ab4ad3191994cfbc2a25b02ec357061e385
Niklaus Maurer <nick.is.normal@gmail.com>
2021-12-12 13:39:44 +0100

    adds logic to open a browser window

--- hello.go
+++ hello.go
@@ -4,9 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/exec"
+	"runtime"
 )
 
 func main() {
+
+	trelloApiKey, present := os.LookupEnv("TRELLO_API_KEY")
+	if !present {
+		log.Fatalf("TRELLO_API_KEY not set")
+	}
+
+	openbrowser(fmt.Sprintf("http://localhost:8080/authorize/?token=%s", trelloApiKey))
 
 	http.HandleFunc("/authorize", func(w http.ResponseWriter, req *http.Request) {
 		query := req.URL.Query()
@@ -18,4 +28,24 @@ func main() {
 	})
 
 	_ = http.ListenAndServe(":8080", nil)
+
+}
+
+func openbrowser(url string) {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
