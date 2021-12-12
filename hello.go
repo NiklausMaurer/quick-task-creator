77ee133953640eab594833ca43ed9510e42d54ec
Niklaus Maurer <nick.is.normal@gmail.com>
2021-12-12 13:15:16 +0100

    removes sendMail

--- hello.go
+++ hello.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/sendgrid/sendgrid-go"
 	"log"
 	"net/http"
-	"os"
-
-	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
 func main() {
@@ -22,39 +18,4 @@ func main() {
 	})
 
 	_ = http.ListenAndServe(":8080", nil)
-}
-
-func sendMail() {
-
-	m := mail.NewV3Mail()
-
-	from := "dev@bychoice.ch"
-	name := "Quick Task Creator"
-	e := mail.NewEmail(name, from)
-	m.SetFrom(e)
-
-	m.SetTemplateID("d-17b13c4e50ac46d3bdcd21ca352d53c6")
-
-	p := mail.NewPersonalization()
-	to := mail.NewEmail("Trello Board", "niklausmaurer+xrh1wnkmzvtdnm56k3au@boards.trello.com")
-	p.AddTos(to)
-
-	p.SetDynamicTemplateData("task", "roll over and survive")
-	p.SetDynamicTemplateData("description", "clean the floor first")
-
-	m.AddPersonalizations(p)
-
-	request := sendgrid.GetRequest(os.Getenv("SENDGRID_API_KEY"), "/v3/mail/send", "https://api.sendgrid.com")
-	request.Method = "POST"
-	var Body = mail.GetRequestBody(m)
-	request.Body = Body
-	response, err := sendgrid.API(request)
-
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(response.StatusCode)
-		fmt.Println(response.Body)
-		fmt.Println(response.Headers)
-	}
 }
