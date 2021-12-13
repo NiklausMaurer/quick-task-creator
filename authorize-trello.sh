855fcaa1f561757075aadec10b3086557da64dcc
Niklaus Maurer <nick.is.normal@gmail.com>
2021-12-13 17:20:53 +0100

    removes authorize-trello.sh

--- authorize-trello.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source trello.env
-
-
-xdg-open "https://trello.com/1/authorize?expiration=1day&name=quick-task-creator&scope=read&response_type=token&key=${TRELLO_API_KEY}"
-
-
-
